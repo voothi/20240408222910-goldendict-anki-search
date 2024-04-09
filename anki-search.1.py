faac17e15a67642c8f7ef39df74e6d3565927f68
Denis Novikov <voothi@gmail.com>
2024-04-09 17:37:35 +0200

    20240409173734

--- anki-search.1.py
+++ anki-search.1.py
@@ -5,12 +5,12 @@ def search_word_in_decks(search_word):
     # Адрес сервера Anki Connect
     anki_connect_url = "http://localhost:8765"
     
-    # Формируем запрос для поиска слова в любой колоде по полю "WordSource"
+    # Формируем запрос для поиска слова в любой колоде без учета полей
     payload = {
         "action": "findNotes",
         "version": 6,
         "params": {
-            "query": f"WordSource:'{search_word}'"
+            "query": search_word
         }
     }
     
@@ -22,26 +22,7 @@ def search_word_in_decks(search_word):
         result = response.json()
         note_ids = result["result"]
         
-        # Теперь формируем запрос для получения всех полей для каждой заметки
-        payload = {
-            "action": "notesInfo",
-            "version": 6,
-            "params": {
-                "notes": note_ids
-            }
-        }
-        
-        # Отправляем запрос для получения всех полей для каждой заметки
-        response = requests.post(anki_connect_url, json=payload)
-        
-        if response.status_code == 200:
-            result = response.json()
-            # Фильтруем только поле "WordDestination" для каждой заметки
-            word_destinations = [note["fields"]["WordDestination"]["value"] for note in result["result"]]
-            return word_destinations
-        else:
-            print("Ошибка при получении всех полей заметок:", response.status_code)
-            return None
+        return note_ids
     else:
         print("Ошибка при отправке запроса для поиска заметок:", response.status_code)
         return None
