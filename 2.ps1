8832691e2410fd7f21d5fe72a9902885d1d630a7
Denis Novikov <voothi@gmail.com>
2024-04-11 09:33:16 +0200

    20240411093248

--- 2.ps1
+++ 2.ps1
@@ -2,7 +2,7 @@ $requestBody = @{
     action = "getDecks"
     version = 6
     params = @{
-        cards = @(1712607760449)
+        cards = @(1712783780711,1712607760452,1712688869426,1712608609451)
     }
 } | ConvertTo-Json
 
