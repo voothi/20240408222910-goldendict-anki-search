84bbaa613b7d3536d5a431938c667f9e642e2f11
Denis Novikov <voothi@gmail.com>
2024-08-31 17:10:05 +0200

    20240831171004

--- anki-search.2.py
+++ anki-search.2.py
@@ -47,7 +47,13 @@ def search_word_in_decks(search_word):
                 word_source = fields.get("WordSource", {}).get("value", None)
                 word_destination = fields.get("WordDestination", {}).get("value", None)
                 sentence_source = fields.get("SentenceSource", {}).get("value", None)
-                card_data.append({"WordSource": word_source, "WordDestination": word_destination, "SentenceSource": sentence_source})
+                deck_name = card.get("deckName", None)  # Извлекаем имя колоды
+                card_data.append({
+                    "WordSource": word_source,
+                    "WordDestination": word_destination,
+                    "SentenceSource": sentence_source,
+                    "DeckName": deck_name  # Добавляем имя колоды
+                })
             return card_data
         else:
             print("Ошибка при получении информации о карточках:", response.status_code)
@@ -72,9 +78,12 @@ if __name__ == "__main__":
                 print(f"{card['WordSource']}", end='')
                 if card['WordDestination']:
                     print(f" – {card['WordDestination']}")
-                else: print("")
+                else:
+                    print("")
             if card['SentenceSource']:
                 print(f"{card['SentenceSource']}")
+            if card['DeckName']:
+                print(f"{card['DeckName']}")
             if i != len(result) - 1:  # Проверяем, не является ли текущая запись последней
                 print("\t")
     else:
